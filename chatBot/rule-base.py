0a33fbdcd190b6aca19a6ba0f51b7d86ce96834a
liaoy19 <liaoy19@uni.coventry.ac.uk>
2020-02-22 18:25:17 +0000

    AA

--- chatBot/rule-base.py
+++ chatBot/rule-base.py
@@ -1,29 +1,28 @@
 import random
-# Greeting
+# greeting
 greetings = ['hola', 'hello', 'hi', 'Hi', 'hey!','hey']
-# GreetingAns
+# 回复打招呼
 random_greeting = random.choice(greetings)
 
-#Question
+# 对于“你怎么样？”这个问题的回复
 question = ['How are you?','How are you doing?']
-# “I am fine”
+# “我很好”
 responses = ['Okay',"I'm fine"]
-# random choice
+# 随机选一个回
 random_response = random.choice(responses)
 
-# run
+# 机器人跑起来
 while True:
     userInput = input(">>> ")
     if userInput in greetings:
         print(random_greeting)
     elif userInput in question:
         print(random_response)
-    # condition of to quit the while loop
+    # 除非你说“拜拜”
     elif userInput == 'bye':
         break
     else:
         print("I did not understand what you said")
-#output:
 # >>> hi
 # hey
 # >>> how are u
