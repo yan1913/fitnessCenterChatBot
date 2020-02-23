ea47f6ddf79162b7f4594a05b9d812aef71d1f22
Yan Liao (liaoy19) <liaoy19@coventry.ac.uk>
2020-02-23 20:22:46 +0000

    Add files via upload

---
+++ chatBot/BMIdemo.py
@@ -0,0 +1,26 @@
+def fun(BMI):
+    if BMI < 15:
+        return "Very severely underweight"
+    elif 15 < BMI < 16:
+        return "Severely underweight"
+    elif 16 < BMI < 18.5:
+        return "Underweight"
+    elif 18.5 < BMI < 25:
+        return "Normal (healthy weight)	"
+    elif 25 < BMI < 30:
+        return "Overweight"
+    elif 30 < BMI < 35:
+        return "Obese Class I (Moderately obese)"
+    elif 35 < BMI < 40:
+        return "Obese Class II (Severely obese)	"
+    else:
+        return "Obese Class III (Very severely obese)	"
+
+def calBMI(height, weight):
+    BMI = weight / (height * height)
+    return BMI
+
+height = float(input("Enter your height:"))
+weight = float(input("Enter your weight:"))
+BMI = float(calBMI(height, weight))
+print(fun(BMI))
