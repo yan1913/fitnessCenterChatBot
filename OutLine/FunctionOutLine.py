636937aac91fcb808595f2c4c47cc0847f4e7afe
liaoy19 <liaoy19@uni.coventry.ac.uk>
2020-02-25 16:28:37 +0000

    AA

---
+++ OutLine/FunctionOutLine.py
@@ -0,0 +1,34 @@
+def greeting():
+    return
+
+
+def register():
+    # if the user is not the membership,then need run the function
+    # to insert the new User's personal information to the memberList
+    return
+
+
+def login():
+    # if the user is the member, need use the function to compare the username and password is correct,
+    #  Then , can do the next step
+    return
+
+
+def BookSportHall():
+    # if the user is the member and login successful, then can run this function to book the sport hall.
+    # also can search the sport hall state(available or not)
+    return
+
+
+def fitnessSuggestion():
+    # costumer can get the personal fitnessSuggestion from this function
+    return
+
+
+def groupExerciseClasses():
+
+    return
+
+
+def BookPhysiotherapy():
+    return
